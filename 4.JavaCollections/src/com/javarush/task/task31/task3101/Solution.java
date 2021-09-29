93d6b407ef1ef10086a573ea274d1eb950151cd6
AleksProv <f1book@mail.ru>
2021-09-29 19:27:06 +0300

    проект 29.09.21

--- 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
@@ -1,10 +1,12 @@
 package com.javarush.task.task31.task3101;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.Map;
 import java.util.TreeMap;
@@ -14,7 +16,59 @@ import java.util.TreeMap;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    Map<String, File> map = new TreeMap<>();
 
+    public static void main(String[] args)  {
+        Solution sol = new Solution();
+        String path = args[0];
+        String resultFileAbsolutePath = args[1];
+
+        try {
+            File resultFile = new File(resultFileAbsolutePath);
+            File dest = new File(resultFile.getParentFile() + "/allFilesContent.txt");
+            if (FileUtils.isExist(dest)) {
+                FileUtils.deleteFile(dest);
+            }
+            FileUtils.renameFile(resultFile, dest);
+
+            Files.walkFileTree(Paths.get(path), new MyVisitor(sol.map));
+
+            for (Map.Entry<String, File> entry : sol.map.entrySet()) {
+                copyFiles(entry.getValue(), dest);
+            }
+        } catch (IOException e) {
+        }
+    }
+
+    public static void copyFiles(File sourceFile, File destFile) {
+        try (FileInputStream fis = new FileInputStream(sourceFile);
+             FileOutputStream fos = new FileOutputStream(destFile, true)) {
+            int i;
+            while ((i = fis.read()) != -1) {
+                fos.write(i);
+            }
+            fos.write("\n".getBytes());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    static class MyVisitor extends SimpleFileVisitor<Path> {
+       public Map<String, File> map;
+
+        public MyVisitor(Map<String, File> map) {
+            this.map = map;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
+            File file = path.toFile();
+            if (file.isFile()) {
+                if (file.length() <= 50) {
+                    map.put(path.toFile().getName(), path.toFile());
+                }
+            }
+            return super.visitFile(path, attrs);
+        }
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
@@ -1,23 +1,38 @@
 package com.javarush.task.task31.task3102;
 
+
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.EnumSet;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /* 
 Находим все файлы
 */
 
 public class Solution {
-    public static List<String> getFileTree(String root) throws IOException {
-        return null;
-
+    public static List<String> getFileTree(String root)  {
+        ArrayList<String> list = new ArrayList<>();
+        Queue<Path> queue = new LinkedList<>();
+        queue.offer(Paths.get(root));
+        while (!queue.isEmpty()){
+            File[] files = new File(queue.poll().toString()).listFiles();
+            for (File file: files){
+                if (file.isFile()){
+                    list.add(file.getAbsolutePath());
+                } else if (file.isDirectory()){
+                    queue.offer(file.toPath());
+                }
+            }
+        }
+        return list;
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args)  {
+        String root = "D:/005";
+        System.out.println(getFileTree(root));
     }
+
 }
